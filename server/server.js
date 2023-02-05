cb8578aef5e0804157aa7db2d6223af6fcfe632c
sojess-work <92088687+sojess-work@users.noreply.github.com>
2023-02-05 10:38:37 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express(),
+      bodyParser = require("body-parser");
+      port = 3080;
+
+
+app.use(bodyParser.json());
+app.use(express.static(process.cwd()+"/authapp-frontend/dist/auth_frontend/"));
+
+
+app.get('/', (req,res) => {
+  res.sendFile(process.cwd()+"/my-app/dist/angular-nodejs-example/index.html")
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on the port::${port}`);
+});
